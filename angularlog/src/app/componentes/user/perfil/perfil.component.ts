188585bf6f9569cc9b58fc6989e0e2cdef456dd8
ec-dani <jd.cantuna@alumnos.upm.es>
2019-12-04 22:26:06 +0100

    buscador añadido

--- angularlog/src/app/componentes/user/perfil/perfil.component.ts
+++ angularlog/src/app/componentes/user/perfil/perfil.component.ts
@@ -14,26 +14,88 @@ export class PerfilComponent implements OnInit {
     private api: ServiceService,
   ) { }
 
-  book: Book;
   books: Book[];
 
   ngOnInit() {
+    console.log(localStorage.getItem("token"));
     this.getAllBooks();
   }
 
   getAllBooks(){
-    this.api.getAllBooks().subscribe((book: Book) => (this.book = book));
+    this.api.getAllBooks().subscribe((book: Book[]) => (this.books = book));
   }
 
   getBookbyTitulo(form){
-    console.log(form.value.titulo);
-    this.api.getBookbyTitle(form.value.titulo).subscribe((data: Book) => {
-      this.book=data;
+    console.log(form);
+    this.api.getBookbyTitle(form.value.input).subscribe((data: Book[]) => {
+      console.log(data);
+      this.books=data;
     },(error: HttpErrorResponse) => {
       if (error.status === 404) {
         alert('Libro no encontrado');
       }
-      this.book= undefined;
     })
+  }
+
+  getBookbyISBN(form){
+    this.api.getBookbyISBN(form.value.input).subscribe((data: Book[]) => {
+      this.books=data;
+    }, (error: HttpErrorResponse) => {
+      if (error.status === 404) {
+        alert('Libro no encontrado');
+      }
+    })
+  }
+
+  getBookbyAutor(form){
+    this.api.getBookbyAutor(form.value.input).subscribe((data: Book[]) => {
+      this.books=data;
+    }, (error: HttpErrorResponse) => {
+      if (error.status === 404) {
+        alert('Libro no encontrado');
+      }
+    })
+  }
+
+  getBookbyFechaPubli(form){
+    this.api.getBookbyFechapubli(form.value.input).subscribe((data: Book[]) => {
+      this.books=data;
+    }, (error: HttpErrorResponse) => {
+      if (error.status === 404) {
+        alert('Libro no encontrado');
+      }
+    })
+  }
+
+  getBookbyEditorial(form){
+    this.api.getBookbyEditorial(form.value.input).subscribe((data: Book[]) => {
+      this.books=data;
+    }, (error: HttpErrorResponse) => {
+      if (error.status === 404) {
+        alert('Libro no encontrado');
+      }
+    })
+  }
+
+  submit(form){
+    if(form.value.opcion === 'Titulo'){
+      this.getBookbyTitulo(form);
+    }
+    else if(form.value.opcion === 'ISBN'){
+      this.getBookbyISBN(form);
+    }
+    else if(form.value.opcion === 'Autor'){
+      this.getBookbyAutor(form);
+    }
+    else if(form.value.opcion === 'Fechapublicacion' ){
+      this.getBookbyFechaPubli(form);
+    }
+    else if(form.value.opcion === 'Editorial'){
+      this.getBookbyEditorial(form);
+    }
+    else if(form.value.opcion === 'Todos'){
+      this.getAllBooks();
+    }
+    
   }
 }
